a65ff78e9d9a50fecc71c653612a3b2381394094
PerfectPan <3207113562@qq.com>
2020-04-28 22:26:20 +0800

    fix

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const octokit = new Octokit({auth: `token ${githubToken}`});
   const gist = await octokit.gists.get({gist_id: gistID})
     .catch(err => {throw new Error(`Get gist failed\n ${err}`)});
   const fileName = Object.keys(gist.data.files)[0];
+  console.log(fileName);
   axios
     .get('https://www.zhihu.com/hot', {
       headers: {
@@ -36,7 +37,7 @@ const octokit = new Octokit({auth: `token ${githubToken}`});
           gist_id: gistID,
           files: {
             [fileName]: {
-              fileName: '知乎热榜',
+              fileName: 'Zhihu',
               content: items.join('\n')
             }
           }
